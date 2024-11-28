ab8a314b419150b01facfa3d10b56c69e0d8ca7b
Matthew Lucas <MatthewThomas.Lucas@nagra.com>
2024-11-28 17:27:35 +0000

    Init templates

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# aoc-2024-in-go
+# 2024 Solutions to The Advent of Code 
+
+This time in Go.
---
+++ day27/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+
+	t.Run("Part 1", func(t *testing.T) {
+
+	})
+
+}
+
+func TestPart2(t *testing.T) {
+
+	t.Run("Part 2", func(t *testing.T) {
+
+	})
+
+}
