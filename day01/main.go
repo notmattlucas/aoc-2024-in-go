ba2b13abca26f4aa68d03bbe5ab199d44781f4b4
Matthew Lucas <lucasmatthew@hotmail.com>
2024-12-01 10:04:01 +0000

    Day01 part1

--- day01/main.go
+++ day01/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"log"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
 func main() {
-	// TODO
+	b, _ := os.ReadFile("./part1_input.txt")
+	result := distance(load(string(b)))
+	log.Printf("Part 1: %d", result)
+}
+
+func load(input string) ([]int, []int) {
+	lines := strings.Split(input, "\n")
+	length := len(lines)
+	xs := make([]int, length)
+	ys := make([]int, length)
+	for idx, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+		fields := strings.Fields(line)
+		xs[idx], _ = strconv.Atoi(fields[0])
+		ys[idx], _ = strconv.Atoi(fields[1])
+	}
+	return xs, ys
+}
+
+func distance(xs []int, ys []int) int {
+	sort.Ints(xs)
+	sort.Ints(ys)
+	var acc float64 = 0
+	for i := 0; i < len(xs); i++ {
+		acc += math.Abs(float64(xs[i] - ys[i]))
+	}
+	return int(acc)
 }
--- day01/main_test.go
+++ day01/main_test.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestPart1(t *testing.T) {
 
-	t.Run("Part 1", func(t *testing.T) {
+	t.Run("Load", func(t *testing.T) {
+		input := `3   4
+		 4   3
+		 2   5
+		 1   3
+		 3   9
+		 3   3`
+		x, y := load(input)
+		assert.Equal(t, []int{3, 4, 2, 1, 3, 3}, x)
+		assert.Equal(t, []int{4, 3, 5, 3, 9, 3}, y)
+	})
 
+	t.Run("Part 1", func(t *testing.T) {
+		assert.Equal(t, 11, distance(
+			[]int{3, 4, 2, 1, 3, 3},
+			[]int{4, 3, 5, 3, 9, 3},
+		))
 	})
 
 }
