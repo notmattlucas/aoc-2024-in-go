f62dd9d4d83a899d41f646ef7898604cb92caecb
Matthew Lucas <MatthewThomas.Lucas@nagra.com>
2024-12-03 08:53:56 +0000

    Day 3

--- day03/main.go
+++ day03/main.go
@@ -1,5 +1,58 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
 func main() {
-	// TODO
+	b, _ := os.ReadFile("./part01_input.txt")
+	ops := parse(string(b))
+	fmt.Printf("Part 1: %d\n", calc(ops))
+	ops = condParse(string(b))
+	fmt.Printf("Part 2: %d\n", calc(ops))
+}
+
+type Mul struct {
+	x int
+	y int
+}
+
+func condParse(input string) []Mul {
+	input = "do()" + input
+	re := regexp.MustCompile(`don't\(\)`)
+	segments := re.Split(input, -1)
+	re = regexp.MustCompile(`do\(\)`)
+	builder := ""
+	for _, segment := range segments {
+		dos := re.Split(segment, -1)[1:]
+		for _, dos := range dos {
+			builder += dos
+		}
+	}
+	return parse(builder)
+}
+
+func calc(ops []Mul) int {
+	sum := 0
+	for _, op := range ops {
+		sum += op.x * op.y
+	}
+	return sum
+}
+
+func parse(input string) []Mul {
+	output := make([]Mul, 0)
+	re := regexp.MustCompile(`mul\(\d+,\d+\)`)
+	segments := re.FindAllString(input, -1)
+	re = regexp.MustCompile(`(\d+)`)
+	for _, segment := range segments {
+		ops := re.FindAllString(segment, -1)
+		x, _ := strconv.Atoi(ops[0])
+		y, _ := strconv.Atoi(ops[1])
+		output = append(output, Mul{x, y})
+	}
+	return output
 }
--- day03/main_test.go
+++ day03/main_test.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestPart1(t *testing.T) {
 
-	t.Run("Part 1", func(t *testing.T) {
+	t.Run("Parse", func(t *testing.T) {
+		ops := parse("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+		assert.Equal(t, []Mul{
+			{2, 4},
+			{5, 5},
+			{11, 8},
+			{8, 5},
+		}, ops)
+	})
 
+	t.Run("Part 1", func(t *testing.T) {
+		ops := parse("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+		sum := calc(ops)
+		assert.Equal(t, 161, sum)
 	})
 
 }
 
 func TestPart2(t *testing.T) {
 
-	t.Run("Part 2", func(t *testing.T) {
+	t.Run("Conditional Parse", func(t *testing.T) {
+		ops := condParse("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+		assert.Equal(t, []Mul{
+			{2, 4},
+			{8, 5},
+		}, ops)
+	})
 
+	t.Run("Part 2", func(t *testing.T) {
+		ops := condParse("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+		sum := calc(ops)
+		assert.Equal(t, 48, sum)
 	})
 
 }
