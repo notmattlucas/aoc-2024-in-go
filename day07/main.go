c5ef5289305a9360b7b0a1429f5c56d6a2555c34
Matthew Lucas <matt.lucas@nagra.com>
2024-12-07 12:11:56 +0000

    Day 7

--- day07/main.go
+++ day07/main.go
@@ -1,5 +1,66 @@
 package main
 
+import (
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Equation struct {
+	total  int
+	opands []int
+}
+
 func main() {
-	// TODO
+	b, _ := os.ReadFile("./part01_input.txt")
+	equations := parse(string(b))
+	log.Printf("Part 1: %d", sumSolvable(equations, false))
+	log.Printf("Part 2: %d", sumSolvable(equations, true))
+}
+
+func sumSolvable(equation []Equation, concat bool) int {
+	total := 0
+	for _, eq := range equation {
+		if reduce(eq.opands, eq.total, concat) {
+			total += eq.total
+		}
+	}
+	return total
+}
+
+func reduce(opands []int, limit int, concat bool) bool {
+	if len(opands) == 1 {
+		return opands[0] == limit
+	}
+	if opands[0] > limit {
+		return false
+	}
+	cat := false
+	if concat && len(opands) > 1 {
+		x := strconv.Itoa(opands[0])
+		y := strconv.Itoa(opands[1])
+		xy, _ := strconv.Atoi(x + y)
+		cat = reduce(append([]int{xy}, opands[2:]...), limit, concat)
+	}
+	add := reduce(append([]int{opands[0] + opands[1]}, opands[2:]...), limit, concat)
+	mul := reduce(append([]int{opands[0] * opands[1]}, opands[2:]...), limit, concat)
+	return add || mul || cat
+}
+
+func parse(input string) []Equation {
+	lines := strings.Split(input, "\n")
+	equations := make([]Equation, 0)
+	for _, line := range lines {
+		line := strings.TrimSpace(line)
+		pair := strings.Split(line, ":")
+		total, _ := strconv.Atoi(strings.TrimSpace(pair[0]))
+		opands := make([]int, 0)
+		for _, s := range strings.Split(strings.TrimSpace(pair[1]), " ") {
+			opand, _ := strconv.Atoi(strings.TrimSpace(s))
+			opands = append(opands, opand)
+		}
+		equations = append(equations, Equation{total, opands})
+	}
+	return equations
 }
--- day07/main_test.go
+++ day07/main_test.go
@@ -1,13 +1,47 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestPart1(t *testing.T) {
 
-	t.Run("Part 1", func(t *testing.T) {
+	t.Run("Parse", func(t *testing.T) {
+		eqs := parse(`190: 10 19
+							3267: 81 40 27
+							83: 17 5
+							156: 15 6
+							7290: 6 8 6 15
+							161011: 16 10 13
+							192: 17 8 14
+							21037: 9 7 18 13
+							292: 11 6 16 20`)
+		assert.Equal(t, []Equation{
+			{190, []int{10, 19}},
+			{3267, []int{81, 40, 27}},
+			{83, []int{17, 5}},
+			{156, []int{15, 6}},
+			{7290, []int{6, 8, 6, 15}},
+			{161011, []int{16, 10, 13}},
+			{192, []int{17, 8, 14}},
+			{21037, []int{9, 7, 18, 13}},
+			{292, []int{11, 6, 16, 20}},
+		}, eqs)
+	})
 
+	t.Run("Part 1", func(t *testing.T) {
+		eqs := parse(`190: 10 19
+							3267: 81 40 27
+							83: 17 5
+							156: 15 6
+							7290: 6 8 6 15
+							161011: 16 10 13
+							192: 17 8 14
+							21037: 9 7 18 13
+							292: 11 6 16 20`)
+		sum := sumSolvable(eqs, false)
+		assert.Equal(t, 3749, sum)
 	})
 
 }
@@ -15,7 +49,17 @@ func TestPart1(t *testing.T) {
 func TestPart2(t *testing.T) {
 
 	t.Run("Part 2", func(t *testing.T) {
-
+		eqs := parse(`190: 10 19
+							3267: 81 40 27
+							83: 17 5
+							156: 15 6
+							7290: 6 8 6 15
+							161011: 16 10 13
+							192: 17 8 14
+							21037: 9 7 18 13
+							292: 11 6 16 20`)
+		sum := sumSolvable(eqs, true)
+		assert.Equal(t, 11387, sum)
 	})
 
 }
