d346fb8d0e471c56976478a446813922f97cf7f9
Matthew Lucas <matt.lucas@nagra.com>
2024-12-14 18:44:20 +0000

    Day 14

--- day14/main.go
+++ day14/main.go
@@ -1,5 +1,154 @@
 package main
 
+import (
+	collections "github.com/notmattlucas/aoc-2024-in-go"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 func main() {
-	// TODO
+	b, _ := os.ReadFile("./part01_input.txt")
+	robots, room := parse(string(b), 101, 103)
+	robots = move(robots, room, 100)
+	log.Printf("Part 1: %d", quadCount(robots, room))
+
+	robots, room = parse(string(b), 101, 103)
+	log.Printf("Part 2: %d", findTree(robots, room, 10000))
+
+}
+
+type Velocity struct{ x, y int }
+
+type Position struct{ x, y int }
+
+type Room struct{ width, height int }
+
+type Positions = [][]bool
+
+func (room Room) luq(pos Position) bool {
+	return pos.x < room.width/2 && pos.y < room.height/2
+}
+
+func (room Room) ruq(pos Position) bool {
+	return pos.x >= (room.width-room.width/2) && pos.y < room.height/2
+}
+
+func (room Room) llq(pos Position) bool {
+	return pos.x < room.width/2 && pos.y >= (room.height-room.height/2)
+}
+
+func (room Room) rlq(pos Position) bool {
+	return pos.x >= (room.width-room.width/2) && pos.y >= (room.height-room.height/2)
+}
+
+func (room Room) positions(robots []Robot) Positions {
+	positions := make(map[Position]bool)
+	for _, robot := range robots {
+		positions[robot.position] = true
+	}
+
+	grid := make([][]bool, room.height)
+	for y := 0; y < room.height; y++ {
+		grid[y] = make([]bool, room.width)
+		for x := 0; x < room.width; x++ {
+			if positions[Position{x, y}] {
+				grid[y][x] = true
+			}
+		}
+	}
+
+	return grid
+}
+
+type Robot struct {
+	position Position
+	velocity Velocity
+}
+
+func (robot Robot) move(room Room, times int) Robot {
+	px := (((robot.position.x + (robot.velocity.x * times)) % room.width) + room.width) % room.width
+	py := (((robot.position.y + (robot.velocity.y * times)) % room.height) + room.height) % room.height
+	return Robot{
+		Position{px, py},
+		robot.velocity,
+	}
+}
+
+func get(positions Positions, x, y int) bool {
+	if x < 0 || y < 0 || y >= len(positions) || x >= len(positions[y]) {
+		return false
+	}
+	return positions[y][x]
+}
+
+func quadCount(robots []Robot, room Room) int {
+	luq, ruq, llq, rlq := 0, 0, 0, 0
+	for _, robot := range robots {
+		pos := robot.position
+		if room.luq(pos) {
+			luq++
+		} else if room.ruq(pos) {
+			ruq++
+		} else if room.llq(pos) {
+			llq++
+		} else if room.rlq(pos) {
+			rlq++
+		}
+	}
+	return luq * ruq * llq * rlq
+}
+
+func move(robots []Robot, room Room, times int) []Robot {
+	moved := make([]Robot, 0)
+	for _, robot := range robots {
+		moved = append(moved, robot.move(room, times))
+	}
+	return moved
+}
+
+func findTree(robots []Robot, room Room, times int) int {
+	for i := 1; i < times; i++ {
+		moved := move(robots, room, i)
+		if hasTreeTop(moved, room) {
+			return i
+		}
+	}
+	return -1
+}
+
+func hasTreeTop(robots []Robot, room Room) bool {
+	positions := room.positions(robots)
+	for _, robot := range robots {
+		pos := robot.position
+		shift := []Velocity{{0, 0},
+			{-1, 1}, {0, 1}, {1, 1},
+			{-2, 2}, {-1, 2}, {0, 2}, {1, 2}, {2, 2}}
+		found, _ := collections.All(shift, func(v Velocity) bool {
+			return get(positions, pos.x+v.x, pos.y+v.y)
+		})
+		if found {
+			return true
+		}
+	}
+	return false
+}
+
+func parse(input string, xmax int, ymax int) ([]Robot, Room) {
+	re := regexp.MustCompile(`(-*\d+)`)
+	robots := make([]Robot, 0)
+	for _, row := range strings.Split(input, "\n") {
+		parts := re.FindAllString(row, 4)
+		px, _ := strconv.Atoi(parts[0])
+		py, _ := strconv.Atoi(parts[1])
+		vx, _ := strconv.Atoi(parts[2])
+		vy, _ := strconv.Atoi(parts[3])
+		robots = append(robots, Robot{
+			Position{px, py},
+			Velocity{vx, vy},
+		})
+	}
+	return robots, Room{xmax, ymax}
 }
--- day14/main_test.go
+++ day14/main_test.go
@@ -1,21 +1,72 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestPart1(t *testing.T) {
 
-	t.Run("Part 1", func(t *testing.T) {
-
+	t.Run("Parse", func(t *testing.T) {
+		robots, room := parse(`p=0,4 v=3,-3
+					p=6,3 v=-1,-3
+					p=10,3 v=-1,2
+					p=2,0 v=2,-1
+					p=0,0 v=1,3
+					p=3,0 v=-2,-2
+					p=7,6 v=-1,-3
+					p=3,0 v=-1,-2
+					p=9,3 v=2,3
+					p=7,3 v=-1,2
+					p=2,4 v=2,-3
+					p=9,5 v=-3,-3`, 11, 7)
+		assert.Equal(t, []Robot{
+			{Position{0, 4}, Velocity{3, -3}},
+			{Position{6, 3}, Velocity{-1, -3}},
+			{Position{10, 3}, Velocity{-1, 2}},
+			{Position{2, 0}, Velocity{2, -1}},
+			{Position{0, 0}, Velocity{1, 3}},
+			{Position{3, 0}, Velocity{-2, -2}},
+			{Position{7, 6}, Velocity{-1, -3}},
+			{Position{3, 0}, Velocity{-1, -2}},
+			{Position{9, 3}, Velocity{2, 3}},
+			{Position{7, 3}, Velocity{-1, 2}},
+			{Position{2, 4}, Velocity{2, -3}},
+			{Position{9, 5}, Velocity{-3, -3}},
+		}, robots)
+		assert.Equal(t, Room{11, 7}, room)
 	})
 
-}
+	t.Run("Move", func(t *testing.T) {
+		room := Room{11, 7}
+		robot := Robot{Position{2, 4}, Velocity{2, -3}}
 
-func TestPart2(t *testing.T) {
+		assert.Equal(t, Robot{Position{4, 1}, Velocity{2, -3}}, robot.move(room, 1))
 
-	t.Run("Part 2", func(t *testing.T) {
+		assert.Equal(t, Robot{Position{6, 5}, Velocity{2, -3}}, robot.move(room, 2))
 
+		assert.Equal(t, Robot{Position{8, 2}, Velocity{2, -3}}, robot.move(room, 3))
+
+		assert.Equal(t, Robot{Position{10, 6}, Velocity{2, -3}}, robot.move(room, 4))
+
+		assert.Equal(t, Robot{Position{1, 3}, Velocity{2, -3}}, robot.move(room, 5))
+	})
+
+	t.Run("Part 01", func(t *testing.T) {
+		robots, room := parse(`p=0,4 v=3,-3
+					p=6,3 v=-1,-3
+					p=10,3 v=-1,2
+					p=2,0 v=2,-1
+					p=0,0 v=1,3
+					p=3,0 v=-2,-2
+					p=7,6 v=-1,-3
+					p=3,0 v=-1,-2
+					p=9,3 v=2,3
+					p=7,3 v=-1,2
+					p=2,4 v=2,-3
+					p=9,5 v=-3,-3`, 11, 7)
+		robots = move(robots, room, 100)
+		assert.Equal(t, 12, quadCount(robots, room))
 	})
 
 }
