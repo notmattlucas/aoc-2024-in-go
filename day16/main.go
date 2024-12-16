197dc38867ddd7b1c6160da51b1df2b547244acd
Matthew Lucas <matt.lucas@nagra.com>
2024-12-16 18:19:46 +0000

    Day 16

--- day16/main.go
+++ day16/main.go
@@ -1,5 +1,132 @@
 package main
 
+import (
+	"log"
+	"math"
+	"os"
+	"slices"
+	"strings"
+)
+
 func main() {
-	// TODO
+	b, _ := os.ReadFile("./part01_input.txt")
+	maze, start, end, velo := parse(string(b))
+	score, path := solve(maze, start, end, velo)
+	log.Printf("Part 1: %d", score)
+	log.Printf("Part 2: %d", len(uniq(path)))
+}
+
+type Maze = [][]string
+
+type Velocity struct {
+	x, y int
+}
+
+type Point struct {
+	x, y int
+}
+
+type State struct {
+	pos  Point
+	velo Velocity
+	cost int
+	path []Point
+}
+
+type Visit struct {
+	pos  Point
+	velo Velocity
+}
+
+func ok(maze Maze, p Point) bool {
+	return maze[p.y][p.x] != "#"
+}
+
+func (state State) move(maze Maze) []State {
+	states := make([]State, 0)
+	fwd := Point{state.pos.x + state.velo.x, state.pos.y + state.velo.y}
+	if ok(maze, fwd) {
+		newpath := make([]Point, len(state.path))
+		copy(newpath, state.path)
+		states = append(states, State{fwd, state.velo, state.cost + 1, append(newpath, state.pos)})
+	}
+	states = append(states, State{state.pos, Velocity{-state.velo.y, state.velo.x}, state.cost + 1000, state.path})
+	states = append(states, State{state.pos, Velocity{state.velo.y, -state.velo.x}, state.cost + 1000, state.path})
+	return states
+}
+
+func (state State) visit() Visit {
+	return Visit{state.pos, state.velo}
+}
+
+func uniq(path []Point) map[Point]bool {
+	seen := map[Point]bool{}
+	for _, p := range path {
+		seen[p] = true
+	}
+	return seen
+}
+
+func solve(maze Maze, start Point, end Point, initial Velocity) (int, []Point) {
+
+	progress := map[Visit]int{}
+	score := math.MaxInt32
+	visited := map[int][]Point{}
+
+	pending := []State{
+		{start, initial, 0, make([]Point, 0)},
+	}
+
+	for len(pending) > 0 {
+
+		head := pending[0]
+		pending = pending[1:]
+
+		// if at end, update score if it was better than previous
+		if head.pos == end {
+			if head.cost != 0 && head.cost <= score {
+				score = head.cost
+				if _, ok := visited[score]; !ok {
+					visited[score] = head.path
+				}
+				visited[score] = slices.Concat(visited[score], head.path, []Point{head.pos})
+			}
+			continue
+		}
+
+		// if we've already had a better score going the same direction, skip
+		prior := progress[head.visit()]
+		if prior != 0 && prior < head.cost {
+			continue
+		}
+
+		// update progress and add new states to pending
+		progress[head.visit()] = head.cost
+		for _, state := range head.move(maze) {
+			pending = append(pending, state)
+		}
+
+	}
+
+	return score, visited[score]
+}
+
+func parse(input string) (Maze, Point, Point, Velocity) {
+	maze := make(Maze, 0)
+	var start, end Point
+	for y, line := range strings.Split(input, "\n") {
+		line = strings.TrimSpace(line)
+		row := make([]string, 0)
+		for x, char := range strings.Split(line, "") {
+			row = append(row, string(char))
+			if char == "S" {
+				start = Point{x, y}
+			}
+			if char == "E" {
+				end = Point{x, y}
+			}
+		}
+		maze = append(maze, row)
+	}
+	return maze, start, end, Velocity{1, 0}
 }
--- day16/main_test.go
+++ day16/main_test.go
@@ -1,21 +1,79 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestPart1(t *testing.T) {
+func TestParts(t *testing.T) {
 
-	t.Run("Part 1", func(t *testing.T) {
-
+	t.Run("Parse", func(t *testing.T) {
+		maze, start, end, velo := parse(`###############
+												#.......#....E#
+												#.#.###.#.###.#
+												#.....#.#...#.#
+												#.###.#####.#.#
+												#.#.#.......#.#
+												#.#.#####.###.#
+												#...........#.#
+												###.#.#####.#.#
+												#...#.....#.#.#
+												#.#.#.###.#.#.#
+												#.....#...#.#.#
+												#.###.#.#.#.#.#
+												#S..#.....#...#
+												###############`)
+		assert.Equal(t, 15, len(maze))
+		assert.Equal(t, 15, len(maze[0]))
+		assert.Equal(t, Point{1, 13}, start)
+		assert.Equal(t, Point{13, 1}, end)
+		assert.Equal(t, Velocity{1, 0}, velo)
 	})
 
-}
+	t.Run("Sample 1", func(t *testing.T) {
+		maze, start, end, velo := parse(`###############
+												#.......#....E#
+												#.#.###.#.###.#
+												#.....#.#...#.#
+												#.###.#####.#.#
+												#.#.#.......#.#
+												#.#.#####.###.#
+												#...........#.#
+												###.#.#####.#.#
+												#...#.....#.#.#
+												#.#.#.###.#.#.#
+												#.....#...#.#.#
+												#.###.#.#.#.#.#
+												#S..#.....#...#
+												###############`)
+		score, path := solve(maze, start, end, velo)
+		assert.Equal(t, 7036, score)
+		u := uniq(path)
+		assert.Equal(t, 45, len(u))
+	})
 
-func TestPart2(t *testing.T) {
-
-	t.Run("Part 2", func(t *testing.T) {
-
+	t.Run("Sample 2", func(t *testing.T) {
+		maze, start, end, velo := parse(`#################
+												#...#...#...#..E#
+												#.#.#.#.#.#.#.#.#
+												#.#.#.#...#...#.#
+												#.#.#.#.###.#.#.#
+												#...#.#.#.....#.#
+												#.#.#.#.#.#####.#
+												#.#...#.#.#.....#
+												#.#.#####.#.###.#
+												#.#.#.......#...#
+												#.#.###.#####.###
+												#.#.#...#.....#.#
+												#.#.#.#####.###.#
+												#.#.#.........#.#
+												#.#.#.#########.#
+												#S#.............#
+												#################`)
+		score, path := solve(maze, start, end, velo)
+		assert.Equal(t, 11048, score)
+		u := uniq(path)
+		assert.Equal(t, 64, len(u))
 	})
 
 }
