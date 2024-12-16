4b005d5fad25f8e363fcbdc39ebb94037fcc7b2c
Matthew Lucas <matt.lucas@nagra.com>
2024-12-16 07:03:12 +0000

    Day 15 (part 1)

--- day15/main.go
+++ day15/main.go
@@ -1,5 +1,227 @@
 package main
 
+import (
+	collections "github.com/notmattlucas/aoc-2024-in-go"
+	"log"
+	"math"
+	"os"
+	"slices"
+	"strings"
+)
+
 func main() {
-	// TODO
+	b, _ := os.ReadFile("./part01_input.txt")
+	objects, mvs, robot, xmax, ymax := parse(string(b), 1)
+	run(objects, mvs, robot)
+	// 1487337
+	log.Printf("Part 1: %d", score(objects))
+
+	objects, mvs, robot, xmax, ymax = parse(string(b), 2)
+	run(objects, mvs, robot)
+	log.Printf("Part 2: %d", score2(objects, xmax, ymax))
+}
+
+type Move struct {
+	x, y int
+}
+type Point = Move
+
+var moves = map[string]Move{
+	"^": {0, -1},
+	"v": {0, 1},
+	"<": {-1, 0},
+	">": {1, 0},
+}
+
+type Object struct {
+	points []Point
+	icon   string
+}
+
+func search(objects []*Object, x, y int) *Object {
+	for _, obj := range objects {
+		if obj.at(x, y) {
+			return obj
+		}
+	}
+	return nil
+}
+
+func (o *Object) at(x, y int) bool {
+	b, _ := collections.Any(o.points, func(p Point) bool {
+		return p.x == x && p.y == y
+	})
+	return b
+}
+
+func (o *Object) closest(xmax int, ymax int) (int, int) {
+	xs := make([]int, 0)
+	ys := make([]int, 0)
+	for _, p := range o.points {
+		xs = append(xs, p.x)
+		ys = append(ys, p.y)
+	}
+	xmn := slices.Min(xs)
+	xmx := slices.Min(xs)
+	ymn := slices.Min(ys)
+	ymx := slices.Min(ys)
+	yscore := math.Min(float64(ymax-ymx), float64(ymn))
+	xscore := math.Min(float64(xmax-xmx), float64(xmn))
+	return int(xscore), int(yscore)
+}
+
+func (o *Object) movable(objects []*Object, velocity Move) bool {
+	next := o.findNext(objects, velocity)
+
+	if o.icon == "#" {
+		return false
+	}
+
+	if len(next) == 0 {
+		return true
+	}
+
+	b, _ := collections.All(next, func(next *Object) bool {
+		return next.movable(objects, velocity)
+	})
+
+	return b
+}
+
+func (o *Object) findNext(objects []*Object, velocity Move) []*Object {
+	next := make([]*Object, 0)
+	for _, p := range o.points {
+		nx := p.x + velocity.x
+		ny := p.y + velocity.y
+		object := search(objects, nx, ny)
+		if object != nil && object != o {
+			next = append(next, object)
+		}
+
+	}
+	return next
+}
+
+func (o *Object) move(objects []*Object, velocity Move) {
+	if !o.movable(objects, velocity) {
+		return
+	}
+
+	next := o.findNext(objects, velocity)
+	for _, nx := range next {
+		nx.move(objects, velocity)
+	}
+
+	npx := make([]Point, 0)
+	for _, p := range o.points {
+		npx = append(npx, Point{p.x + velocity.x, p.y + velocity.y})
+	}
+	o.points = npx
+}
+
+func run(objects []*Object, mvs []Move, robot *Object) {
+	for _, mv := range mvs {
+		(*robot).move(objects, mv)
+	}
+}
+
+func score(objects []*Object) int {
+	total := 0
+	for _, obj := range objects {
+		if obj.icon == "O" {
+			for _, p := range obj.points {
+				total += p.y*100 + p.x
+			}
+		}
+	}
+	return total
+}
+
+func score2(objects []*Object, xmax int, ymax int) int {
+	total := 0
+	for _, obj := range objects {
+		if obj.icon == "O" {
+			x, y := obj.closest(xmax, ymax)
+			total += y*100 + x
+		}
+	}
+	return total
+}
+
+func parse(input string, width int) ([]*Object, []Move, *Object, int, int) {
+	lines := strings.Split(input, "\n")
+	var robot Object
+	var objects []*Object
+	var mvs []Move
+	ymax := 0
+	xmax := 0
+	for y, line := range lines {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		if strings.HasPrefix(line, "<") || strings.HasPrefix(line, ">") || strings.HasPrefix(line, "^") || strings.HasPrefix(line, "v") {
+			for _, c := range line {
+				mvs = append(mvs, moves[string(c)])
+			}
+		} else {
+			x := 0
+			ymax += 1
+			for _, c := range strings.Split(line, "") {
+
+				points := make([]Point, 0)
+				for i := 0; i < width; i++ {
+					points = append(points, Point{x + i, y})
+				}
+
+				switch c {
+				case "#":
+					objects = append(objects, &Object{points, c})
+				case "O":
+					objects = append(objects, &Object{points, c})
+				case "@":
+					{
+						robot = Object{[]Point{{x, y}}, c}
+						objects = append(objects, &robot)
+					}
+				}
+
+				xmax = x
+				x += width
+			}
+		}
+	}
+	return objects, mvs, &robot, xmax, ymax
+}
+
+func print(objects []*Object) string {
+	xmax := 0
+	ymax := 0
+	for _, object := range objects {
+		for _, point := range object.points {
+			xmax = int(math.Max(float64(xmax), float64(point.x)))
+			ymax = int(math.Max(float64(ymax), float64(point.y)))
+		}
+	}
+	grid := make([][]string, ymax+1)
+	for y := 0; y <= ymax; y++ {
+		grid[y] = make([]string, xmax+1)
+		for x := 0; x <= xmax; x++ {
+			grid[y][x] = "."
+			for _, object := range objects {
+				if object.at(x, y) {
+					grid[y][x] = object.icon
+				}
+			}
+		}
+	}
+
+	s := ""
+	for y := 0; y <= ymax; y++ {
+		for x := 0; x <= xmax; x++ {
+			s += grid[y][x]
+		}
+		s += "\n"
+	}
+	return s
 }
--- day15/main_test.go
+++ day15/main_test.go
@@ -1,21 +1,136 @@
 package main
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestPart1(t *testing.T) {
 
-	t.Run("Part 1", func(t *testing.T) {
+	t.Run("Parse", func(t *testing.T) {
+		_, mvs, _, _, _ := parse(`########
+				#..O.O.#
+				##@.O..#
+				#...O..#
+				#.#.O..#
+				#...O..#
+				#......#
+				########
+				
+				<^^>>>vv<v>>v<<`, 1)
 
+		assert.Equal(t, []Move{
+			{-1, 0},
+			{0, -1},
+			{0, -1},
+			{1, 0},
+			{1, 0},
+			{1, 0},
+			{0, 1},
+			{0, 1},
+			{-1, 0},
+			{0, 1},
+			{1, 0},
+			{1, 0},
+			{0, 1},
+			{-1, 0},
+			{-1, 0},
+		}, mvs)
+
+	})
+
+	t.Run("Part 1 Small", func(t *testing.T) {
+		grid, mvs, pos, xmax, ymax := parse(`########
+				#..O.O.#
+				##@.O..#
+				#...O..#
+				#.#.O..#
+				#...O..#
+				#......#
+				########
+				
+				<^^>>>vv<v>>v<<`, 1)
+		run(grid, mvs, pos)
+		assert.Equal(t, 7, xmax)
+		assert.Equal(t, 8, ymax)
+		assert.Equal(t, 2028, score(grid))
+	})
+
+	t.Run("Part 1 Large", func(t *testing.T) {
+		grid, mvs, pos, _, _ := parse(`##########
+									#..O..O.O#
+									#......O.#
+									#.OO..O.O#
+									#..O@..O.#
+									#O#..O...#
+									#O..O..O.#
+									#.OO.O.OO#
+									#....O...#
+									##########
+									
+									<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+									vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+									><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+									<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+									^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+									^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+									>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+									<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+									^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+									v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^`, 1)
+		run(grid, mvs, pos)
+		assert.Equal(t, 10092, score(grid))
 	})
 
 }
 
 func TestPart2(t *testing.T) {
 
-	t.Run("Part 2", func(t *testing.T) {
+	t.Run("Part 2 Small", func(t *testing.T) {
+		grid, mvs, pos, xmax, ymax := parse(`#######
+									#...#.#
+									#.....#
+									#..OO@#
+									#..O..#
+									#.....#
+									#######
+									
+									<vv<<^^<<^^`, 2)
+		fmt.Println(print(grid))
+		run(grid, mvs, pos)
+		fmt.Println(print(grid))
+		assert.Equal(t, 14, xmax)
+		assert.Equal(t, 7, ymax)
+		//assert.Equal(t, 105, score2(grid, xmax, ymax))
+	})
 
+	t.Run("Part 2 Large", func(t *testing.T) {
+		grid, mvs, pos, xmax, ymax := parse(`##########
+									#..O..O.O#
+									#......O.#
+									#.OO..O.O#
+									#..O@..O.#
+									#O#..O...#
+									#O..O..O.#
+									#.OO.O.OO#
+									#....O...#
+									##########
+									
+									<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+									vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+									><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+									<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+									^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+									^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+									>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+									<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+									^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+									v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^`, 2)
+		fmt.Println(print(grid))
+		run(grid, mvs, pos)
+		fmt.Println(print(grid))
+		assert.Equal(t, 9021, score2(grid, xmax, ymax))
 	})
 
 }
