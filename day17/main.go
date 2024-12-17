1cdeb57c4879dcb5ff4ae32eac33ad75507ea214
Matthew Lucas <matt.lucas@nagra.com>
2024-12-17 18:07:53 +0000

    Day 17 (the bit I could be bothered with)

--- day17/main.go
+++ day17/main.go
@@ -1,5 +1,158 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 func main() {
-	// TODO
+	cmp := parse(`Register A: 51571418
+						Register B: 0
+						Register C: 0
+						
+						Program: 2,4,1,1,7,5,0,3,1,4,4,5,5,5,3,0
+						`)
+	output := cmp.Run()
+	log.Printf("Part 1: %s", strings.Trim(strings.Join(strings.Fields(fmt.Sprint(output)), ","), "[]"))
+}
+
+type Computer struct {
+	instructs []int
+	instruct  int
+	registers map[string]int
+}
+
+func NewComputer(instructions []int) Computer {
+	return Computer{
+		instructs: instructions,
+		instruct:  0,
+		registers: map[string]int{
+			"a": 0,
+			"b": 0,
+			"c": 0,
+		},
+	}
+}
+
+func (c *Computer) Run() []int {
+	output := make([]int, 0)
+	for c.instruct < len(c.instructs) {
+		op := c.instructs[c.instruct]
+		opand := c.instructs[c.instruct+1]
+
+		var progress bool
+		var result []int
+
+		switch op {
+		case 0:
+			progress, result = c.adv(opand)
+		case 1:
+			progress, result = c.bxl(opand)
+		case 2:
+			progress, result = c.bst(opand)
+		case 3:
+			progress, result = c.jnz(opand)
+		case 4:
+			progress, result = c.bxc(opand)
+		case 5:
+			progress, result = c.out(opand)
+		case 6:
+			progress, result = c.bdv(opand)
+		case 7:
+			progress, result = c.cdv(opand)
+		}
+
+		output = append(output, result...)
+		if progress {
+			c.instruct += 2
+		}
+	}
+	return output
+}
+
+func (c *Computer) GetRegister(name string) int {
+	return c.registers[name]
+}
+
+func (c *Computer) SetRegister(name string, val int) {
+	c.registers[name] = val
+}
+
+func (c *Computer) combo(opand int) int {
+	if opand < 4 {
+		return opand
+	}
+	return c.GetRegister([]string{"a", "b", "c"}[opand-4])
+}
+
+func (c *Computer) adv(opand int) (bool, []int) {
+	denominator := math.Pow(2, float64(c.combo(opand)))
+	val := float64(c.GetRegister("a")) / denominator
+	c.SetRegister("a", int(val))
+	return true, []int{}
+}
+
+func (c *Computer) bxl(opand int) (bool, []int) {
+	c.SetRegister("b", c.GetRegister("b")^opand)
+	return true, []int{}
+}
+
+func (c *Computer) bst(opand int) (bool, []int) {
+	c.SetRegister("b", c.combo(opand)%8)
+	return true, []int{}
+}
+
+func (c *Computer) jnz(opand int) (bool, []int) {
+	if c.GetRegister("a") != 0 {
+		c.instruct = opand
+		return false, []int{}
+	}
+	return true, []int{}
+}
+
+func (c *Computer) bxc(_ int) (bool, []int) {
+	c.SetRegister("b", c.GetRegister("b")^c.GetRegister("c"))
+	return true, []int{}
+}
+
+func (c *Computer) out(opand int) (bool, []int) {
+	return true, []int{c.combo(opand) % 8}
+}
+
+func (c *Computer) bdv(opand int) (bool, []int) {
+	val := c.GetRegister("a") / int(math.Pow(2, float64(c.combo(opand))))
+	c.SetRegister("b", val)
+	return true, []int{}
+}
+
+func (c *Computer) cdv(opand int) (bool, []int) {
+	val := c.GetRegister("a") / int(math.Pow(2, float64(c.combo(opand))))
+	c.SetRegister("c", val)
+	return true, []int{}
+}
+
+func parse(input string) Computer {
+	lines := strings.Split(input, "\n")
+	re := regexp.MustCompile(`(\d+)`)
+	a, _ := strconv.Atoi(re.FindAllString(lines[0], -1)[0])
+	b, _ := strconv.Atoi(re.FindAllString(lines[1], -1)[0])
+	c, _ := strconv.Atoi(re.FindAllString(lines[2], -1)[0])
+	instructs := make([]int, 0)
+	for _, num := range re.FindAllString(lines[4], -1) {
+		n, _ := strconv.Atoi(num)
+		instructs = append(instructs, n)
+	}
+	return Computer{
+		instructs: instructs,
+		instruct:  0,
+		registers: map[string]int{
+			"a": a,
+			"b": b,
+			"c": c,
+		},
+	}
 }
--- day17/main_test.go
+++ day17/main_test.go
@@ -1,13 +1,68 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestPart1(t *testing.T) {
 
-	t.Run("Part 1", func(t *testing.T) {
+	t.Run("If register C contains 9, the program 2,6 would set register B to 1.", func(t *testing.T) {
+		computer := NewComputer([]int{2, 6})
+		computer.SetRegister("c", 9)
+		computer.Run()
+		assert.Equal(t, 1, computer.GetRegister("b"))
+	})
 
+	t.Run("If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.", func(t *testing.T) {
+		computer := NewComputer([]int{5, 0, 5, 1, 5, 4})
+		computer.SetRegister("a", 10)
+		output := computer.Run()
+		assert.Equal(t, []int{0, 1, 2}, output)
+	})
+
+	t.Run("If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.", func(t *testing.T) {
+		computer := NewComputer([]int{0, 1, 5, 4, 3, 0})
+		computer.SetRegister("a", 2024)
+		output := computer.Run()
+		assert.Equal(t, 0, computer.GetRegister("a"))
+		assert.Equal(t, []int{4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0}, output)
+	})
+
+	t.Run("If register B contains 29, the program 1,7 would set register B to 26.", func(t *testing.T) {
+		computer := NewComputer([]int{1, 7})
+		computer.SetRegister("b", 29)
+		computer.Run()
+		assert.Equal(t, 26, computer.GetRegister("b"))
+	})
+
+	t.Run("If register B contains 2024 and register C contains 43690, the program 4,0 would set register B to 44354.", func(t *testing.T) {
+		computer := NewComputer([]int{4, 0})
+		computer.SetRegister("b", 2024)
+		computer.SetRegister("c", 43690)
+		computer.Run()
+		assert.Equal(t, 44354, computer.GetRegister("b"))
+	})
+
+	t.Run("Parse", func(t *testing.T) {
+		cmp := parse(`Register A: 729
+					Register B: 0
+					Register C: 0
+					
+					Program: 0,1,5,4,3,0`)
+		assert.Equal(t, 729, cmp.GetRegister("a"))
+		assert.Equal(t, 0, cmp.GetRegister("b"))
+		assert.Equal(t, 0, cmp.GetRegister("c"))
+		assert.Equal(t, []int{0, 1, 5, 4, 3, 0}, cmp.instructs)
+	})
+
+	t.Run("Part 1", func(t *testing.T) {
+		cmp := parse(`Register A: 729
+					Register B: 0
+					Register C: 0
+					
+					Program: 0,1,5,4,3,0`)
+		assert.Equal(t, []int{4, 6, 3, 5, 6, 3, 5, 2, 1, 0}, cmp.Run())
 	})
 
 }
