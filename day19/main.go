db043cc5cf5fb000eb43484c1781b0713348e3bd
Matthew Lucas <matt.lucas@nagra.com>
2024-12-19 16:07:13 +0000

    Day 19

--- day19/main.go
+++ day19/main.go
@@ -1,5 +1,59 @@
 package main
 
+import (
+	"log"
+	"os"
+	"strings"
+)
+
 func main() {
-	// TODO
+	b, _ := os.ReadFile("./part01_input.txt")
+	patterns, designs := parse(string(b))
+	count, sum := nsatisfied(designs, patterns)
+	log.Printf("Part 1: %d", count)
+	log.Printf("Part 2: %d", sum)
+}
+
+func nsatisfied(designs []string, patterns []string) (int, int) {
+	count := 0
+	sum := 0
+	for _, design := range designs {
+		sat := satisfied(&map[string]int{}, design, patterns)
+		if sat > 0 {
+			count++
+		}
+		sum += sat
+	}
+	return count, sum
+}
+
+func satisfied(cache *map[string]int, design string, patterns []string) int {
+	if value, ok := (*cache)[design]; ok {
+		return value
+	}
+
+	if len(design) == 0 {
+		return 1
+	}
+
+	count := 0
+	for _, pattern := range patterns {
+		if strings.HasPrefix(design, pattern) {
+			count += satisfied(cache, design[len(pattern):], patterns)
+		}
+	}
+
+	(*cache)[design] = count
+
+	return count
+}
+
+func parse(input string) ([]string, []string) {
+	lines := strings.Split(input, "\n")
+	patterns := strings.Split(lines[0], ", ")
+	designs := make([]string, 0)
+	for _, line := range lines[2:] {
+		designs = append(designs, strings.TrimSpace(line))
+	}
+	return patterns, designs
 }
--- day19/main_test.go
+++ day19/main_test.go
@@ -1,13 +1,53 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestPart1(t *testing.T) {
 
-	t.Run("Part 1", func(t *testing.T) {
+	t.Run("Parse", func(t *testing.T) {
+		patterns, designs := parse(`r, wr, b, g, bwu, rb, gb, br
 
+					brwrr
+					bggr
+					gbbr
+					rrbgbr
+					ubwu
+					bwurrg
+					brgr
+					bbrgwb`)
+
+		assert.Equal(t, []string{
+			"r", "wr", "b", "g", "bwu", "rb", "gb", "br",
+		}, patterns)
+		assert.Equal(t, []string{
+			"brwrr",
+			"bggr",
+			"gbbr",
+			"rrbgbr",
+			"ubwu",
+			"bwurrg",
+			"brgr",
+			"bbrgwb",
+		}, designs)
+	})
+
+	t.Run("Part 1", func(t *testing.T) {
+		patterns, designs := parse(`r, wr, b, g, bwu, rb, gb, br
+
+					brwrr
+					bggr
+					gbbr
+					rrbgbr
+					ubwu
+					bwurrg
+					brgr
+					bbrgwb`)
+
+		count, _ := nsatisfied(designs, patterns)
+		assert.Equal(t, 6, count)
 	})
 
 }
@@ -15,7 +55,19 @@ func TestPart1(t *testing.T) {
 func TestPart2(t *testing.T) {
 
 	t.Run("Part 2", func(t *testing.T) {
+		patterns, designs := parse(`r, wr, b, g, bwu, rb, gb, br
 
+					brwrr
+					bggr
+					gbbr
+					rrbgbr
+					ubwu
+					bwurrg
+					brgr
+					bbrgwb`)
+
+		_, sum := nsatisfied(designs, patterns)
+		assert.Equal(t, 16, sum)
 	})
 
 }
